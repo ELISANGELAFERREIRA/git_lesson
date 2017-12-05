a9c91282982f90b001cda5ddbf9862ce11ee416c
Elisângela Ferreira <elisange.f@hotmail.com>
2017-12-05 20:18:23 -0200

    First Commit

---
+++ program.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Program
+{
+
+	public static void Main(string[] args)
+	{
+
+		Console.WriteLine ("Hello World");
+
+		if (args != null)
+		{
+			Console.WriteLine("Args:");
+			foreach(var arg in args)
+			{
+				Console.WriteLine(args);
+			}
+
+		}
+	}
+}
+
